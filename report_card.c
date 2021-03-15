08908e1cb98240345045b6a58538fe6932e5ff8d
lastCarnival <danieljyh@gmail.com>
2021-03-15 21:01:29 +0900

    society

--- report_card.c
+++ report_card.c
@@ -4,10 +4,12 @@ int main() {
 	int korean = 80;
 	int english = 90;
 	int math =70;
+	int society = 10;
 
 	printf("한글점수: %d", korean);
 	printf("영어점수: %d", english);
 	printf("수학점수: %d", math);
+	printf("사회점수: $d", society);
 	
 	return 0;
 }
