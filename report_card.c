0e483c6959b365d92df0feb6d7077a1092cc468b
lastCarnival <danieljyh@gmail.com>
2021-03-15 20:48:16 +0900

    report card: Add base code

---
+++ report_card.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int korean = 80;
+	int english = 90;
+	int math =70;
+
+	printf("한글점수: %d", korean);
+	printf("영어점수: %d", english);
+	printf("수학점수: %d", math);
+	
+	return 0;
+}
+	
+
